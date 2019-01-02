e9c1991b3e138b3bb7a577597779d62cf3f5a228
Tomoyuki Hagiwara <hagiwara.t.tam@tam-tam.co.jp>
2019-01-02 15:54:23 +0900

    access /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ var express = require('express')
 var bodyParser = require('body-parser')
 
 var { mongoose } = require('./db/mongoose.js') // mongoose
+const { ObjectID } = require('mongodb')
+
 var { Todo } = require('./models/todo.js')
 var { User } = require('./models/user.js')
 
@@ -33,6 +35,27 @@ app.get('/todos', (req, res) => {
     res.status(400).send(err)
   })
 })
+
+// /todosにパラメータ付きでアクセス
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id
+
+  // idの部分がidの形式にあっていない場合
+  if( !ObjectID.isValid(id) ) {
+    res.status(404).send()
+  }
+
+  Todo.findById(id).then(( todo ) => {
+    // 該当するデータがない場合
+    if ( !todo ) {
+      res.status(404).send()
+    }
+    res.send( {todo} )
+  }).catch(( err ) => {
+    res.status(400).send(err)
+  })
+})
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000 ........')
