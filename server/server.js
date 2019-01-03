3960ef5cdae3f209aaadb8d9f59da0320e3c0dba
Tomoyuki Hagiwara <hagiwara.t.tam@tam-tam.co.jp>
2019-01-03 11:47:48 +0900

    todos patch

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-var express = require('express')
-var bodyParser = require('body-parser')
+const _ = require('lodash')
+const express = require('express')
+const bodyParser = require('body-parser')
 
-var { mongoose } = require('./db/mongoose.js') // mongoose
+const { mongoose } = require('./db/mongoose.js') // mongoose
 const { ObjectID } = require('mongodb')
 
-var { Todo } = require('./models/todo.js')
-var { User } = require('./models/user.js')
+const { Todo } = require('./models/todo.js')
+const { User } = require('./models/user.js')
 
-var app = express()
+const app = express()
 const port = process.env.PORT || 3000 // herokuであれば環境変数使用、localでは3000
 
 app.use(bodyParser.json())
@@ -76,6 +77,42 @@ app.delete('/todos/:id', (req, res) => {
     res.send( {todo} )
   }).catch(( err ) => {
     return res.status(400).send()
+  })
+})
+
+
+// 更新
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id
+  var body = _.pick(req.body, ['text', `completed`]) // 更新するプロパティをオブジェクトで取得
+
+  // idの形式が不正
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  // completedがbooleanの形式 && 入力値がtrue
+  if (_.isBoolean(body.completed) && body.completed) {
+    // completedAtを現在時間で更新
+    body.completedAt = new Date().getTime()
+  } else {
+    // completedがfalseなので、falseで更新
+    body.completed = false
+    body.completedAt = null
+  }
+
+  Todo.findByIdAndUpdate(
+    id, // 更新するid
+    { $set: body }, // 更新する値
+    { new: true } // 更新後の結果を返す
+  ).then((todo) => {
+    if (!todo) { // 値がなかった場合
+      return res.status(404).send()
+    }
+
+    res.send({todo})
+  }).catch(( err ) => {
+    res.status(400).send()
   })
 })
 
