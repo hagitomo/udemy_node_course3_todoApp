ea301a0147c6e5a6ffa11dda8936d49166bc37ae
Tomoyuki Hagiwara <hagiwara.t.tam@tam-tam.co.jp>
2019-01-05 00:37:26 +0900

    logout test

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -338,3 +338,24 @@ describe('POST /users/login', () => {
       })
   })
 })
+
+// ログアウト処理のテスト
+describe('Delete /users/me/token', () => {
+  it('should remove token logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end(( err, res ) => {
+        if( err ) {
+          return done(err)
+        }
+
+        // userから検索しても該当するトークンを持つユーザーは存在しない
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0)
+          done()
+        }).catch((err) => done(err))
+      })
+  })
+})
