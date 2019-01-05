454773b9dea62a4562954ef65b8c15d55729bc8d
Tomoyuki Hagiwara <hagiwara.t.tam@tam-tam.co.jp>
2019-01-05 15:11:42 +0900

    edit config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,11 @@
 // 環境変数
 const env = process.env.NODE_ENV  || 'development' // (heroku上だと`production`に)
 
-if (env === 'development') {
-  process.env.PORT = 3000
-  process.env.MONGODB_URI = 'mongodb://user:user@localhost:27017/TodoApp'
-} else if (env === 'test' ) { // test実行時にenvを`test`に設定してある（package.json）
-  process.env.PORT = 3000
-  process.env.MONGODB_URI = 'mongodb://user:user@localhost:27017/TodoAppTest'
+if (env === 'development' || env === 'test' ) {
+  var config = require('./config.json')
+  var envConfig = config[env]
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key]
+  })
 }
